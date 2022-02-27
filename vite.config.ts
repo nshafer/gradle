ceb9f5e90f4252a93a2c60cd10a7c4ccdf1810f9
Nathan Shafer <nate@lotech.org>
2022-02-27 14:59:33 -0700

    PWA stuff

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,40 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        VitePWA({
+            includeAssets: [
+                'favicon.ico', 'robots.txt', 'apple-touch-icon.png',
+                '*.svg', '*.png'],
+            manifest: {
+                "name": "Wordle Helper",
+                "short_name": "Wordle Helper",
+                "description": "Help you solve Wordle",
+                "theme_color": "#ffffff",
+                "icons": [
+                    {
+                        "src": "/android-chrome-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png"
+                    },
+                    {
+                        "src": "/android-chrome-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png"
+                    }
+                ],
+
+            }
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
