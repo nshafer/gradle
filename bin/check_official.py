61dcc5494e94ae54bca3dd180d2e916a55164519
Nathan Shafer <nate@lotech.org>
2022-09-02 15:20:41 -0700

    Script to check for changes to word lists

---
+++ bin/check_official.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import re
+import requests
+
+from words import answers, valid
+
+
+def get(url):
+    r = requests.get(url)
+    r.raise_for_status()
+    return r.text
+
+
+# Parses a given JS script to find an array with the given words in it
+def parse_array(script, words):
+    words_re = r"[^\[\]]*".join(words)
+    array_match = re.search(r'\[([^\[\]]*' + words_re + r'[^\[\]]*)\]', script)
+    if array_match:
+        # print(f"Found array: {array_match.group(1)[:100]}...")
+        words = array_match.group(1).split(",")
+        return list(map(lambda w: w.strip(' \n\t"'), words))
+
+
+def compare_arrays(type, a, b):
+    a_set = set(a)
+    b_set = set(b)
+
+    removed_words = a_set.difference(b_set)
+    added_words = b_set.difference(a_set)
+
+    if removed_words:
+        print(f"[{type}] removed words: {removed_words}")
+    if added_words:
+        print(f"[{type}] added words: {added_words}")
+
+
+def main():
+    page = get("https://www.nytimes.com/games/wordle/index.html")
+
+    script_match = re.search(r'src="([^"]*wordle\.?[^"]*\.js[^"]*)', page)
+    if script_match:
+        # print(f"Found wordle script: {script_match.group(1)}")
+        script = get(script_match.group(1))
+        js_answers = parse_array(script, ["karma", "irony", "fever"])
+        if not js_answers:
+            print("Could not find answers in script")
+            return
+        js_valid = parse_array(script, ["arval", "kaugh", "tolar"])
+        if not js_valid:
+            print("Could not find valid words in script")
+            return
+
+        compare_arrays("answers", answers, js_answers)
+        compare_arrays("valid", valid, js_valid)
+    else:
+        print("ERROR: could not find JS script")
+
+
+if __name__ == "__main__":
+    main()
+
+
